7fdc2dbe5abc0657e902a721e02373c1ab551d95
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-08-21 12:46:45 +0000

    [Add]ログインリンクまでbootstrap反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,10 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 
-import "bootstrap"
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
 
 Rails.start()
 Turbolinks.start()
