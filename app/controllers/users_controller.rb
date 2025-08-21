58ad9bee6a6e89972313d87445eb940934edfdfb
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-08-21 05:59:09 +0000

    [Add]テンプレート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @book.books
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 
 end
