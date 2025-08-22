ae64f1b6a7eb20ce14b17d3c6d4b6da3c034a67d
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-08-22 06:18:20 +0000

    [Add]BootstrapUserInfo反映途中

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  layout 'logged_in'
+
+  def index
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @user = User.new
+  end
+
+
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])
@@ -10,11 +15,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+   if @user.update(user_params)
+    redirect_to user_path(@user)
+   else
+    render :edit
   end
 
   private
@@ -22,5 +31,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-
+end
 end
