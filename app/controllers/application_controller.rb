8824f9c3eef2189da5852bfeee7d08f288b63753
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-08-23 09:49:26 +0000

    [Add]プロフ編集設定、Usersページ作成、Showリンク途中まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "successfully"
-    my_books_path
+    stored_location_for(resource) || my_books_path
   end
 
   def after_sign_out_path_for(resource)
