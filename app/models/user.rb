4fbca7265504ebc4fd349d83fbd1f7782379a77f
Shirasu0703 <219084816+Shirasu0703@users.noreply.github.com>
2025-08-23 16:13:30 +0000

    [Add]Rspecテストまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  has_many :books, dependent: :destroy
   #has_one_attached :image
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
          authentication_keys: [:name]
-         has_many :books, dependent: :destroy
+
 
   has_one_attached :profile_image
 
