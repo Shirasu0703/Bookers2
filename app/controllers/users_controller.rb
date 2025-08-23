2e81c82a5421c88dc08fd84253ccf50a5442594a
Shirasu0703 <219084816+Shirasu0703@users.noreply.github.com>
2025-08-23 02:01:05 +0000

    [Add]ShowIndexUserIndexMyBooksページ追加中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page])
+    
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   get 'books/top'
   get 'homes/top'
   devise_for :users
+  get "books/my_books", to: "books#my_books", as: :my_books
   resources :users, only: [:show, :index, :edit, :update, :create]
   
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
   get "homes/about" => "homes#about", as: "about"
+
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
